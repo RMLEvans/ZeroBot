1f95357433f43a6c0b9b1c9cbc8ac76f19467998
R Mike Evans <llewelyn.email@gmail.com>
2017-08-09 21:22:04 +0100

    Defined separate counters for left & right wheels so they can be controlled separately e.g. to turn on the spot.

--- zerobot.py
+++ zerobot.py
@@ -14,7 +14,7 @@ IN1_m2 = g0.OutputDevice(6)
 StepPins_m2 = [IN1_m2,IN2_m2,IN3_m2,IN4_m2] # Motor 2 GPIO pins
 Seq = [[0,0,1,1],  # Define step sequence.
        [0,0,1,0],  # Stepper motor contains four magnetic coils, 90 degrees apart.
-       [0,1,1,0],  # Either a single coil or two adjacent coils can be activated,
+       [0,1,1,0],  # Either a single coil or two adjacent coils can be activated, 
        [0,1,0,0],  # so 8 positions are possible, 45 degrees apart.
        [1,1,0,0],  # Activating them in sequence causes rotation.
        [1,0,0,0],  # Note there is gearing inside each motor unit (ratio 512:1 ?).
@@ -24,45 +24,48 @@ StepCount = len(Seq)
 all_clear = True
 running = True
 
-def bump_watch(): # thread to watch fro obstacles
+def bump_watch(): # thread to watch for obstacles
     global all_clear
     while running:
-        value = sensor.Measurement(20, 16, 20, 'metric', 1)
+        value = sensor.Measurement(20, 16, 20, 'metric', 1)   # (trig_pin, echo_pin, temperature, unit, round_to)
         if value.raw_distance() < 10: # trigger if obstacle within 10cm
             all_clear = False
         else:
             all_clear = True
 
 def move_bump(direction='F', motorSpeed=1, numsteps=2052):
-    counter = 0 # 2052 steps = 1 revolution for stepsize of 2
-    StepDir = motorSpeed # Set to 1 or 2 for fwd, -1 or -2 for back
-    if direction == 'B':
-        StepDir = StepDir * -1
-    WaitTime = 10/float(1000) # adjust this to change speed
-    StepCounter = 0
+    WaitTime = 10/float(10000) # adjust this to change speed
+	if direction == 'L' or direction == 'F':
+		leftStepSize = motorSpeed # Set to 1, 2 or 3 for forwards rotation. 3 is fastest (skipping motor positions, but lowest power.
+	else:
+		leftStepSize = -motorSpeed
+	if direction == 'R' or direction == 'F':
+		rightStepSize = motorSpeed
+	else:
+		rightStepSize = -motorSpeed
+    leftStepCounter = 0
+    rightStepCounter = 0
+    counter = 0 # 4104 steps = 1 revolution
     while all_clear and counter < numsteps: # only move if no obstacles
-      for pin in range(0, 4):
-        Lpin = StepPins_m1[pin]
-        Rpin = StepPins_m2[pin]
-        if Seq[StepCounter][pin]!=0: # F = fwd, B=back, L=left, R=right
-          if direction == 'L' or direction == 'B' or direction == 'F':
-            Lpin.on() # Left wheel only
-          if direction == 'R' or direction == 'B' or direction == 'F':
-            Rpin.on() # Right wheel only
-        else:
-          Lpin.off()
-          Rpin.off()
-      StepCounter += StepDir
-      if (StepCounter>=StepCount):  # Repeat sequence
-        StepCounter = 0
-      if (StepCounter<0):
-        StepCounter = StepCount+StepDir
-      time.sleep(WaitTime)  #pause
-      counter+=motorSpeed
+		leftStepCounter = (leftStepCounter+leftStepSize+StepCount)%StepCount
+		rightStepCounter = (rightStepCounter+rightStepSize+StepCount)%StepCount
+		for pin in range(0, 4):
+			Lpin = StepPins_m1[pin]
+			Rpin = StepPins_m2[pin]
+			if Seq[leftStepCounter][pin]==1:
+				Lpin.on()
+			else:
+				Lpin.off()
+			if Seq[rightStepCounter][pin]==1:
+				Rpin.on() # Right wheel only
+			else:
+				Rpin.off()
+		time.sleep(WaitTime)  #pause
+		counter += motorSpeed
 
 t1 = Thread(target=bump_watch) # run as seperate thread
 t1.start() # start bump watch thread
-for i in range(4): # Draw a right-handes square
+for i in range(4): # Draw a right-handed square
     move_bump('F',3,4104)
     move_bump('R',3,2052)
 running = False
